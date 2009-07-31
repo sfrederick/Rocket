ee8c380d002bbebb569f0fa56bb14d23265587e0
sfrederick <stacie@alum.calberkeley.org>
2009-07-31 00:58:54 -0700

    Minor changes to rocket/index login, logout

--- app/controllers/rocket_controller.rb
+++ app/controllers/rocket_controller.rb
@@ -1,28 +1,25 @@
 class RocketController < ApplicationController
 
   def index
-    redirect_to :action => "login"
-  end
-
-  def login
-    if request.post?
+    if request.post? && session[:user_id] == nil
       user = User.authenticate(params[:name], params[:password])
       if user
         session[:user_id] = user.id
-        redirect_to :action => "status"
+        # redirect_to :controller => "status", :action => "list"
+        redirect_to :action => "index"
       else
         flash.now[:notice] = "Invalid user/password combination"
       end
     end
   end
 
+  def status
+  end
+
   def logout
     session[:user_id] = nil
     flash[:notice] = "Logged out"
-    redirect_to :action => "login"
+    redirect_to :action => "index"
   end
 
-  def status
-    
-  end
 end
